3503a867f2c904c4d16f8b601cf06a4ef6c27698
MicroFox_HD <75223426+MicroFox-HD@users.noreply.github.com>
2021-12-20 18:47:28 +0100

    Update roles.lua

--- roles.lua
+++ roles.lua
@@ -3,10 +3,11 @@ local roles = {
 		"MicroFox_HD",
 		"Fire_Face1231",
 		"Rafaelis4",
+		"masteryRuben",
+    		"trycokitten666",
 	},
 	["vips"] = {
 		"TigeyCal",
-    		"trycokitten666",
     		"Fire_Hunter9313",
 	},
 }
