51691f5a27ec0ae3476c06f609f083a0d61a59dc
MicroFox_HD <75223426+MicroFox-HD@users.noreply.github.com>
2021-12-21 00:08:13 +0100

    Update roles.lua

--- roles.lua
+++ roles.lua
@@ -1,10 +1,12 @@
 local roles = {
 	["sadmins"] = {
 		"MicroFox_HD",
+		"MicroFox_Storage",
 		"trycokitten666",
 	},
 	["admins"] = {
 		"MicroFox_HD",
+		"MicroFox_Storage",
 		"Fire_Face1231",
 		"Rafaelis4",
 		"masteryRuben",
